5e253acd2be0ffcc273e5cd79ad8035520035dfc
MATEUS FRANÇA DE VASCONCELOS <jshshsnsn@gmail.com>
2025-03-16 10:54:44 -0300

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -14,7 +14,7 @@ st.markdown('# FIFA 23 OFFICIAL DATASET ⚽')
 st.sidebar.markdown("Desenvolvido por Mateus França")
 st.markdown('O conjunto de dados contém uma ampla gama de atributos, incluindo dados demográficos do jogador, características físicas, estatísticas de jogo, detalhes do contrato e afiliações ao clube. Com mais de 17.000 registros, este conjunto de dados oferece um recurso valioso para analistas, pesquisadores e entusiastas do futebol interessados ​​em explorar vários aspectos do mundo do futebol, pois permite estudar atributos do jogador, métricas de desempenho, avaliação de mercado, análise do clube, posicionamento do jogador e desenvolvimento do jogador ao longo do tempo.')
 
-btn = st.button('Acesse os Dados no Kaggle')
+btn = st.link_button('Acesse os Dados no Kaggle')
 if btn:
     webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
     
